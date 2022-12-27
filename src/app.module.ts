4028bb5c66c653ed84c4290e33921802e36dca04
Renato Niño <jorgenino07@gmail.com>
2022-12-27 00:14:02 -0500

    Recursos creados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClienteModule } from './cliente/cliente.module';
+import { ProductoModule } from './producto/producto.module';
+import { VentaModule } from './venta/venta.module';
 
 @Module({
-  imports: [],
+  imports: [ClienteModule, ProductoModule, VentaModule],
   controllers: [AppController],
   providers: [AppService],
 })
